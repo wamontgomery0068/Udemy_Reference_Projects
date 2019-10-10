2d7f95bac7039fed96ad98de12c6259fd20cc62a
Your Name <wamontgomery0068@gmail.com>
2019-10-10 12:39:34 -0600

    Protecting Routes with canActivate

--- routing-start/src/app/app.routing.module.ts
+++ routing-start/src/app/app.routing.module.ts
@@ -8,6 +8,7 @@ import { UserComponent } from "./users/user/user.component";
 import { ServersComponent } from "./servers/servers.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     // Home Page
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
       { path: ':id/:name', component: UserComponent },
     ] },
     // Servers Page 
-    { path: 'servers', component: ServersComponent, children: [
+    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
         // Single Server Page 
       { path: ':id', component: ServerComponent }, 
       // Edit Server Page
