8c1498d46f79c0853919e76d39956194b3db77cf
Your Name <wamontgomery0068@gmail.com>
2019-10-11 09:10:21 -0600

    Note added

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// Imported Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -10,12 +11,16 @@ import { UserComponent } from './users/user/user.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app.routing.module';
+
+// Imported Services
+import { ServersService } from './servers/servers.service';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+
+// Imported Modules
+import { AppRoutingModule } from './app.routing.module';
 
 
 @NgModule({
