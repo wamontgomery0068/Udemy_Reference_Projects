7d8a5716899912fe14ca9c9c540e7fe19c9e165c
Your Name <wamontgomery0068@gmail.com>
2019-10-14 10:10:32 -0600

    Adding Child Routing Together

--- prj-services-final/src/app/app.module.ts
+++ prj-services-final/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app.routing.module';
@@ -25,6 +26,7 @@ import { AppRoutingModule } from './app.routing.module';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipeStartComponent,
     DropdownDirective
   ],
   imports: [
