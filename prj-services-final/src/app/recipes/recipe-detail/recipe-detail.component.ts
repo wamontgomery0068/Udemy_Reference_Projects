823bd671ddeab66febdc80c2c8182719ae31f9af
Your Name <wamontgomery0068@gmail.com>
2019-10-14 10:28:17 -0600

    Configuring Route Parameters

--- prj-services-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-services-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -19,6 +19,7 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params ['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
     })
   }
 
