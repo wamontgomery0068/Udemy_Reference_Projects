8aa7be9a3cc948edecc35c67c1466d56a46e8828
Your Name <wamontgomery0068@gmail.com>
2019-10-15 14:44:44 -0600

    Set Default Values with ngModel Property Binding

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultQuestion = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
