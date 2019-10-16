5efd21da81f6667e7eee3c954f0e21d9f373e329
Your Name <wamontgomery0068@gmail.com>
2019-10-16 10:09:27 -0600

    Hello World Challenge and Common Errors

--- Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
+++ Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
@@ -1,8 +1,17 @@
 public class Hello {
 
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hakuna Matata");
+//
+//    }
+
+    // Challenge
+    // Instructions:
+    // How would you go about changing this to print my name, instead of World?
+
     public static void main(String[] args) {
-
-        System.out.println("Hakuna Matata");
-
+        // System.out.println("Hello, " + "Andrew");
+        System.out.println("Hello, Andrew");
     }
 }
