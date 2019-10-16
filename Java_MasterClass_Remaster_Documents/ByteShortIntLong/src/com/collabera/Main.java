e98f0b1cb9ff14a02e145d2bdd2d91b89c70d23b
Your Name <wamontgomery0068@gmail.com>
2019-10-16 13:43:39 -0600

    byte_short_long_width

--- Java_MasterClass_Remaster_Documents/ByteShortIntLong/src/com/collabera/Main.java
+++ Java_MasterClass_Remaster_Documents/ByteShortIntLong/src/com/collabera/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // ***** Int Example *****
+
         int myValue = 10000;
 
         int myMiniIntValue = Integer.MIN_VALUE;
@@ -16,6 +18,35 @@ public class Main {
 
         int myMaxInTest = 2147483647;
 
+        // ***** Byte Example *****
 
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        // ***** Short Example *****
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        // ***** Long Example *****
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2147483647234L;
+        System.out.println("bigLongLiteralValue: " + bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
     }
 }
