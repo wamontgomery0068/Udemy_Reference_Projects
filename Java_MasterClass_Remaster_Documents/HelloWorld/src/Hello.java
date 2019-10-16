e5c2595a0c567dce64e793e57db6c82c2a8f8fd7
Your Name <wamontgomery0068@gmail.com>
2019-10-16 10:38:47 -0600

    Variables

--- Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
+++ Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
@@ -10,8 +10,24 @@ public class Hello {
     // Instructions:
     // How would you go about changing this to print my name, instead of World?
 
+//    public static void main(String[] args) {
+//        System.out.println("Hello, " + "Andrew");
+//        System.out.println("Hello, Andrew");
+//
+//        int myFirstNumber = 5;
+//
+//    }
+
+    // Challenge
+    // Instructions:
+    // Looking at the first System.out.println statement, what do you think should be typed on the
+    // line below our declaration statement to print out the value of the variable myFirstNumber?
     public static void main(String[] args) {
-        // System.out.println("Hello, " + "Andrew");
         System.out.println("Hello, Andrew");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println("myFirstNumber: " + myFirstNumber);
+
     }
+
 }
