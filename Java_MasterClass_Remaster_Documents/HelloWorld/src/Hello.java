9ea8821167771ac488e0508eede9c69a5394bd63
Your Name <wamontgomery0068@gmail.com>
2019-10-16 12:10:22 -0600

    Starting out with Expressions

--- Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
+++ Java_MasterClass_Remaster_Documents/HelloWorld/src/Hello.java
@@ -26,7 +26,14 @@ public class Hello {
         System.out.println("Hello, Andrew");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println("myFirstNumber: " + myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("myTotal: " + myTotal);
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println("myLastOne: " + myLastOne);
 
     }
 
