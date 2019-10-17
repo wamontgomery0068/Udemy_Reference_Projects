267e15c83475ac48b7f7c0c22bd2448a0911ea86
Your Name <wamontgomery0068@gmail.com>
2019-10-17 10:02:49 -0600

    The char and boolean Primitive Data Types

---
+++ Java_MasterClass_Remaster_Documents/CharAndBoolean/src/com/collabera/Main.java
@@ -0,0 +1,24 @@
+package com.collabera;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // *** Char Primitive Type ***
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        // *** Boolean Primitive Type ***
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
