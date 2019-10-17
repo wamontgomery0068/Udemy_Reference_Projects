3ec1ba089bd90c3627d3b4ea8afab2ee113b8219
Your Name <wamontgomery0068@gmail.com>
2019-10-17 07:18:30 -0600

    float and double Primitive Types

---
+++ Java_MasterClass_Remaster_Documents/FloatAndDouble/src/com/collabera/Main.java
@@ -0,0 +1,28 @@
+package com.collabera;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ***** Float Example *****
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("myMinFloatValue: " + myMinFloatValue);
+        System.out.println("myMaxFloatValue: " + myMaxFloatValue);
+
+        // ***** Double Example *****
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("myMinDoubleValue: " + myMinDoubleValue);
+        System.out.println("myMaxDoubleValue: " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5d;
+
+    }
+}
