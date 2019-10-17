46c77a9204c09eb190f9510d6d69fda8ed4d60d7
Your Name <wamontgomery0068@gmail.com>
2019-10-17 07:44:14 -0600

    Floating Point Precision and a Challenge

--- Java_MasterClass_Remaster_Documents/FloatAndDouble/src/com/collabera/Main.java
+++ Java_MasterClass_Remaster_Documents/FloatAndDouble/src/com/collabera/Main.java
@@ -20,9 +20,32 @@ public class Main {
         System.out.println("myMinDoubleValue: " + myMinDoubleValue);
         System.out.println("myMaxDoubleValue: " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+
+        System.out.println("myIntValue: " + myIntValue);
+
+        System.out.println("myFloatValue: " + myFloatValue);
+
+        System.out.println("myDoubleValue: " + myDoubleValue);
+
+        // ***** Challenge *****
+        // Instructions
+        // ** 1) Create a variable with the appropriate type to store the number of pounds to be converted to kilograms
+        // ** 2) Calculate the result i.e. the number of kilograms based on the contents of the variable
+        // ** above and store the result in a 2nd appropriate variable.
+        // ** 3) Print out the result.
+
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms: " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+
+        System.out.println(pi);
+        System.out.println(anotherNumber);
 
     }
 }
