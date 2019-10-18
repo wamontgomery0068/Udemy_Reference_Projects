a594e85fa9525265e083a2dcb35f0704e4c4862f
Your Name <wamontgomery0068@gmail.com>
2019-10-18 14:46:12 -0600

    Keywords and Expressions

---
+++ Java_MasterClass_Remaster_Documents/KeywordsAndExpressions/src/com/collabera/Main.java
@@ -0,0 +1,34 @@
+package com.collabera;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 43. Keywords and Expressions -- Code Below
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+
+        // An expression consist of variables, values and operators.
+
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // In the following code that I will type below, write down what parts of the code
+        // are expressions
+
+        int score = 100;
+        if(score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+        // 1) score = 100;
+        // 2) score > 99
+        // 3) "You got the high score!"
+        // 4) score = 0;
+
+    }
+}
