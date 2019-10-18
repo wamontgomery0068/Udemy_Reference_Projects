34d7f858e443d368e65bec23852fd16ce401cd4c
Your Name <wamontgomery0068@gmail.com>
2019-10-18 10:48:18 -0600

    Logical and Operator

--- Java_MasterClass_Remaster_Documents/Operators/src/com/collabera/Main.java
+++ Java_MasterClass_Remaster_Documents/Operators/src/com/collabera/Main.java
@@ -53,5 +53,15 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if(topScore < 100) {
+            System.out.println("You got the highest score.");
+        }
+
+        int secondScore = 8;
+        if (topScore > secondScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
     }
 }
