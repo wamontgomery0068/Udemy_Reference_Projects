eeff6b38641537f50c6bfa82ac21055b93768e82
Your Name <wamontgomery0068@gmail.com>
2019-10-21 12:00:18 -0600

    Methods in Java

--- Java_MasterClass_Remaster_Documents/Methods/src/com/collabera/Main.java
+++ Java_MasterClass_Remaster_Documents/Methods/src/com/collabera/Main.java
@@ -9,18 +9,31 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was  " + finalScore);
-        }
+        // Execute the calculateScore method
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
+        // Execute the calculateScore method
+        calculateScore(gameOver, score,levelCompleted, bonus);
+
+    }
+
+    // Create a method that will calculate the score
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 2000;
+            System.out.println("Your final score was  " + finalScore);
+
+            return finalScore;
         }
+
+        return -1;
+
+
     }
 }
