e45affa04c4d0bf62c71c9035adef7958f228b2e
Your Name <wamontgomery0068@gmail.com>
2019-10-24 08:54:02 -0600

    Consumer Exception without DLX

---
+++ RabbitMQ_&_Java_(Spring_Boot)_for_System_Integration/rabbitmq-consumer/src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureImageConsumer.java
@@ -0,0 +1,33 @@
+package com.course.rabbitmqconsumer.consumer;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import com.course.rabbitmqconsumer.entity.Picture;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class MyPictureImageConsumer {
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	private static final Logger log = LoggerFactory.getLogger(MyPictureImageConsumer.class);
+	
+	@RabbitListener(queues = "q.mypicture.image")
+	public void listen(String message) throws JsonParseException, JsonMappingException, IOException {
+		var p = objectMapper.readValue(message, Picture.class);
+		
+		if(p.getSize() > 9000) {
+			throw new IllegalArgumentException("Picture size too large : " + p);
+		}
+		
+		log.info("On image : {}", p.toString());
+	}
+
+}
