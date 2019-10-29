b6549bae8321b063f6d1559aa72f152f61c145bf
Your Name <wamontgomery0068@gmail.com>
2019-10-29 07:21:29 -0600

    Commit

---
+++ Java_MasterClass_Remaster_Documents/StatementsWhiteSpaceAndIndenting/src/com/collabera/Main.java
@@ -0,0 +1,13 @@
+package com.collabera;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test!");
+
+    }
+}
